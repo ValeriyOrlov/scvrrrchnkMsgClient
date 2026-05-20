807f2bed9b0f6a611667637dec6dc382cd80ff14
Valerrriy Orlov <wrestlbird@gmail.com>
2026-05-20 22:37:54 +0300

    feat: реализован хук для обёртки вызовов API и автоматического обновления стора

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,36 @@
+import { useCallback } from "react";
+import useAuthStore from "../store/authStore";
+import { loginApi, registerApi } from "../lib/api";
+import { jwtDecode } from "jwt-decode";
+
+export function useAuth() {
+  const { accessToken, user, setAuth, clearAuth } = useAuthStore()
+
+  const login = useCallback(async (email: string, password: string ) => {
+    const data = await loginApi(email, password)
+    const payload: any = jwtDecode(data.access_token)
+    const user = {
+      id: payload.user_id,
+      username: payload.username,
+    }
+    setAuth(data.access_token, data.refresh_token, user)
+  }, [setAuth])
+
+  const register = useCallback(async (email: string, username: string, password: string) => {
+    await registerApi(email, username, password)
+    await login(email, password)
+  }, [login])
+
+  const logout = useCallback(() => {
+    clearAuth()
+  }, [clearAuth])
+
+  return {
+    accessToken,
+    user,
+    isAuthenticated: !!accessToken,
+    login,
+    register,
+    logout,
+  }
+}
