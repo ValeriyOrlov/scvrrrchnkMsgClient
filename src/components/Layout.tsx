74f3290790d5cd26c653a54d6cea19e31926c870
Valerrriy Orlov <wrestlbird@gmail.com>
2026-05-23 10:09:17 +0300

    fix: скорректирован цвет фона

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div className="max-w-lg mx-auto min-h-screen bg-gray-50 p-4
+    <div className="max-w-lg mx-auto min-h-screen bg-white p-4
     ">
       <Outlet />
     </div>
