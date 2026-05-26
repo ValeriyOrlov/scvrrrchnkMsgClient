737de755eea0c000105693135580ea6fc771fd2f
Valerrriy Orlov <wrestlbird@gmail.com>
2026-05-26 20:25:37 +0300

    feat: добавлена обёртка для работы react query

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,23 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css"
 import App from "./App.tsx"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,  // не перезапрашивать при фокусе окна (для мобильных удобно)
+      retry: 1,                     // одна повторная попытка при ошибке
+    },
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 )
