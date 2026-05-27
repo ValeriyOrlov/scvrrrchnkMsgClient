07b86fe0233d677f7141b0ae82a40003965578a4
Valerrriy Orlov <wrestlbird@gmail.com>
2026-05-27 21:41:18 +0300

    feat: внедрили хук вебсокета

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom";
+import { useWebSocket } from "../hooks/useWebSocket";
 
 export default function Layout() {
+  useWebSocket()
   return (
     <div className="max-w-lg mx-auto min-h-screen bg-white p-4
     ">
