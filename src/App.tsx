e7158cff4a8a4e48bd8ab6ba76b69e4cbb57829e
Valerrriy Orlov <wrestlbird@gmail.com>
2026-06-03 22:24:30 +0300

    feat: добавлены маршруты страниц информации о чате и создания групповых чатов

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import Layout from "./components/Layout.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
 import RegisterPage from "./pages/RegisterPage.tsx";
 import ChatsPage from "./pages/ChatsPage.tsx";
-import ChatRoomPage from "./pages/ChatsRoomPage.tsx";
+import ChatRoomPage from "./pages/ChatRoomPage.tsx";
 import SearchPage from "./pages/SearchPage.tsx";
+import SearchGroupMembersPage from "./pages/SearchGroupMembersPage.tsx";
+import ChatInfoPage from "./pages/ChatInfoPage.tsx";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path="/chats" element={<ChatsPage />} />
         <Route path="/chats/:id" element={<ChatRoomPage />} />
         <Route path="/search" element={<SearchPage />} />
+        <Route path="/searchGroupMembers" element={<SearchGroupMembersPage />} />
+        <Route path="/chats/:id/info" element={<ChatInfoPage />} />
         {/* Корневой путь автоматически перенаправляет на /chats */}
         <Route path="/" element={<Navigate to="/chats" replace />} />
       </Route>
