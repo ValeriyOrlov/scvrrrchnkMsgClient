a96bb0b4e2c0a7350ba18193ba3d0f3e81748760
Valerrriy Orlov <wrestlbird@gmail.com>
2026-06-14 20:53:02 +0300

    feat: реализована рассылка события typing при наборе сообщения

--- src/components/MessageInput.tsx
+++ src/components/MessageInput.tsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { useSendMessage } from '../hooks/useSendMessage'
+import { useWS } from '../contexts/WebSocketContext'
 
 interface Props {
   chatId: number
 }
 
 export default function MessageInput({ chatId }: Props) {
+  const { sendMessage } = useWS()
+  console.log('sendMessage available:', !!sendMessage)
+
   const [text, setText] = useState('')
   const { mutate, isPending } = useSendMessage(chatId)
+  const lastTypingSent = useRef(0)
 
   const handleSend = () => {
     if (!text.trim() || isPending) return
@@ -16,12 +21,24 @@ export default function MessageInput({ chatId }: Props) {
     })
   }
 
+    const handleTyping = () => {
+    const now = Date.now()
+    if (now - lastTypingSent.current > 2000) {
+      lastTypingSent.current = now
+      console.log('Sending typing event')
+      sendMessage({ type: 'typing', chat_id: chatId })
+    }
+  }
+
   return (
     <div className="p-4 border-t border-gray-200 flex gap-2">
       <input
         type="text"
         value={text}
-        onChange={(e) => setText(e.target.value)}
+        onChange={(e) => {
+          setText(e.target.value)
+          handleTyping()
+        }}
         onKeyDown={(e) => e.key === 'Enter' && handleSend()}
         placeholder="Сообщение..."
         className="flex-1 border border-gray-300 rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
